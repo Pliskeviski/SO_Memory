2d441f7ec965a34fa4611d6b33df6b07ac9d48ad
Gustavo Pliskeviski <gustavopliskeviski@gmail.com>
2019-08-30 20:12:00 -0300

    Update exec.sh

--- ED_SO/exec.sh
+++ ED_SO/exec.sh
@@ -1,3 +1,3 @@
 rm ./a.out
-nice -n -18 g++ main.cpp GerarArquivo/GerarArquivo.cpp GerarScript/GerarScript.cpp Helper/MenuMemoria.cpp Memoria/Algoritmo.cpp Memoria/Algoritmos/BestFit.cpp Memoria/Algoritmos/FirstFit.cpp Memoria/Algoritmos/WorstFit.cpp Memoria/Algoritmos/QuickFit.cpp -std=c++14 -w
+nice -n -18 g++ main.cpp GerarArquivo/GerarArquivo.cpp GerarScript/GerarScript.cpp Helper/MenuMemoria.cpp Memoria/Algoritmo.cpp Memoria/Algoritmos/BestFit.cpp Memoria/Algoritmos/FirstFit.cpp Memoria/Algoritmos/WorstFit.cpp -std=c++14 -w
 ./a.out -r
